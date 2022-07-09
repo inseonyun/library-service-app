aff372e887e594b0e5688ac7d0116ec147e25673
inseonyun <yuninseon@naver.com>
2022-07-09 19:03:51 +0900

    유저 책 반납 기능 구현 완료

--- library-service-administrator/app/src/main/java/com/library_service_administrator/Adapter_User_info.kt
+++ library-service-administrator/app/src/main/java/com/library_service_administrator/Adapter_User_info.kt
@@ -1,12 +1,16 @@
 package com.library_service_administrator
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Adapter_User_info (val context: Context, val bookList: ArrayList<List_User_info>) : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -22,6 +26,29 @@ class Adapter_User_info (val context: Context, val bookList: ArrayList<List_User
         book_Name.text = user.BookName.toString()
         book_LoanDate.text = user.BookLoanDate.toString()
         book_ReturnDate.text = user.BookReturnDate.toString()
+
+        // 버튼 이벤트 처리
+        book_status.setOnClickListener {
+            API_Return_Book.create().post_input(
+                    user.UserID,
+                    user.BookName.toString()
+            ).enqueue(object: Callback<PostLoanReturnResult> {
+                override fun onResponse(call: Call<PostLoanReturnResult>, response: Response<PostLoanReturnResult>) {
+                    Log.i("Request Info", "Return Book Success!!!")
+
+                    if(!response.body().toString().isNullOrEmpty()) {
+                        // 리스트뷰 갱신
+                       bookList.removeAt(position)
+                        notifyDataSetChanged()
+                    }
+                }
+
+                override fun onFailure(call: Call<PostLoanReturnResult>, t: Throwable) {
+                    Log.i("Request Info", "Search Book Failed!!!")
+                    Log.e("Request Error", t.toString())
+                }
+            })
+        }
 
         return view
     }
---
+++ library-service-administrator/app/src/main/java/com/library_service_administrator/PostLoanReturnResult.kt
@@ -0,0 +1,5 @@
+package com.library_service_administrator
+
+data class PostLoanReturnResult(
+    var result: String? = null
+)
