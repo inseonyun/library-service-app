6cde4df2ca3908b9ed07a6845b7bccb537842eb6
inseonyun <yuninseon@naver.com>
2022-07-19 23:22:49 +0900

    로그인 화면 구성만 짜놓음

---
+++ library-service-administrator/app/src/main/java/com/library_service_administrator/Fragment_Login.kt
@@ -0,0 +1,57 @@
+package com.library_service_administrator
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.library_service_administrator.databinding.FragmentLoginBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class Fragment_Login : Fragment() {
+    // ViewBinding
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    // Retrofit API
+    val api_login = API_Login.create()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // login 버튼 클릭 이벤트 처리
+        binding.btnLogin.setOnClickListener {
+            val id = binding.etId.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            api_login.post_input(
+                id,
+                password
+            ).enqueue(object: Callback<PostLoginResult> {
+                override fun onResponse(call: Call<PostLoginResult>, response: Response<PostLoginResult>) {
+                    Log.i("Request Info", "Login User Success!!!")
+
+                    if(!response.body().toString().isNullOrEmpty()) {
+                        Toast.makeText(context, response.body()?.content.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<PostLoginResult>, t: Throwable) {
+                    Log.i("Request Info", "Login User Failed!!!")
+                    Log.e("Request Error", t.toString())
+                }
+            })
+
+        }
+
+        return view
+    }
+}
---
+++ library-service-administrator/app/src/main/java/com/library_service_administrator/PostLoginResult.kt
@@ -0,0 +1,6 @@
+package com.library_service_administrator
+
+data class PostLoginResult(
+    var flag: Boolean? = null,
+    var content: String? = null
+)
